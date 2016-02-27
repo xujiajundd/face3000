f9dc99741358189f851c4807bf90732304d30d30
xujiajun <xujiajun@yeecall.com>
2016-02-27 14:40:10 +0800

    Initial Commit

---
+++ face3000/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  face3000
+//
+//  Created by 徐家骏 on 16/2/27.
+//  Copyright © 2016年 徐家骏. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
