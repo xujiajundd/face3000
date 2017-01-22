8aa853d3c087d590135c8fb8b72b86d629b41b62
Xu Jiajun <xujiajun@yeecall.com>
2017-01-22 13:34:00 +0800

    s0122-6

--- face3000/main.cpp
+++ face3000/main.cpp
@@ -638,7 +638,7 @@ void Train(const char* ModelName){
     params.detect_factor_by_stage_.push_back(0.2);
     
     params.tree_depth_ = 3;
-    params.trees_num_per_forest_ = 5;
+    params.trees_num_per_forest_ = 6;
     params.initial_guess_ = 2;
 
 //    params.group_num_ = 6;
--- face3000/regressor.cpp
+++ face3000/regressor.cpp
@@ -97,8 +97,8 @@ void CascadeRegressor::Train(std::vector<cv::Mat_<uchar> >& images,
                 BoundingBox ibox = bboxes_[i];
                 float minor = random_generator.uniform(-ibox.width, ibox.width);
                 float minor1 = random_generator.uniform(-ibox.width, ibox.width);
-                minor = 0.08 * minor;
-                minor1 = 0.08 * minor1;
+                minor = 0.1 * minor;
+                minor1 = 0.1 * minor1;
                 ibox.start_x -= minor/2.0;
                 ibox.start_y -= minor/2.0;
                 ibox.width += minor1;
@@ -321,7 +321,7 @@ void CascadeRegressor::Train(std::vector<cv::Mat_<uchar> >& images,
 //                            std::cout << "image index:" << augmented_images_index[j] << " tryTimes:" << tryTimes << " ee:" << ee << std::endl;
                         }
                     }
-                } while ( tryTimes < 50); //这个地方可能会死循环的
+                } while ( tryTimes < 60); //这个地方可能会死循环的
             }
         }
         
