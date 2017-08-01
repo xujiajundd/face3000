e63630d9f536613067bdbbc0945ad336b3f7d912
Xu Jiajun <xujiajun@yeecall.com>
2017-08-01 18:25:27 +0800

    s0801

--- face3000/main.cpp
+++ face3000/main.cpp
@@ -638,11 +638,11 @@ void Train(const char* ModelName){
 //    params.local_radius_by_stage_.push_back(0.05);
 //    params.local_radius_by_stage_.push_back(0.03);
     
-    params.detect_factor_by_stage_.push_back(0.8);
     params.detect_factor_by_stage_.push_back(0.7);
     params.detect_factor_by_stage_.push_back(0.6);
+    params.detect_factor_by_stage_.push_back(0.5);
     params.detect_factor_by_stage_.push_back(0.4);
-    params.detect_factor_by_stage_.push_back(0.1);
+    params.detect_factor_by_stage_.push_back(0.3);
     params.detect_factor_by_stage_.push_back(0.9);
     params.detect_factor_by_stage_.push_back(0.4);
     params.detect_factor_by_stage_.push_back(0.2);
