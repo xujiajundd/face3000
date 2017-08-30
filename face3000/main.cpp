40820d6588ccc17747ad218fda0603fba55a0478
Xu Jiajun <xujiajun@yeecall.com>
2017-08-30 17:52:54 +0800

    s0830-d3n8

--- face3000/main.cpp
+++ face3000/main.cpp
@@ -641,13 +641,13 @@ void Train(const char* ModelName){
     params.detect_factor_by_stage_.push_back(0.7);
     params.detect_factor_by_stage_.push_back(0.6);
     params.detect_factor_by_stage_.push_back(0.5);
-    params.detect_factor_by_stage_.push_back(0.4);
-    params.detect_factor_by_stage_.push_back(0.3);
+    params.detect_factor_by_stage_.push_back(0.5);
+    params.detect_factor_by_stage_.push_back(0.5);
     params.detect_factor_by_stage_.push_back(0.9);
     params.detect_factor_by_stage_.push_back(0.4);
     params.detect_factor_by_stage_.push_back(0.2);
     
-    params.tree_depth_ = 4;
+    params.tree_depth_ = 3;
     params.trees_num_per_forest_ = 8;
     params.initial_guess_ = 2;
 
