b34476cb20cfcf25879b850a21b40139af44874a
Xu Jiajun <xujiajun@yeecall.com>
2017-09-28 14:45:30 +0800

    s0929

--- face3000/randomforest.cpp
+++ face3000/randomforest.cpp
@@ -1392,17 +1392,17 @@ RandomForest::RandomForest(Parameters& param, int landmark_index, int stage, std
             local_features_num_ = param.local_features_num_;
         }
         else{
-            local_features_num_ = param.local_features_num_ / 4;
+            local_features_num_ = param.local_features_num_;
         }
     }
 //    else if ( stage >= 3 ){
 //        local_features_num_ = param.local_features_num_ / 2;
 //    }
     else if ( stage == 1 || stage == 2 ){
-        local_features_num_ = param.local_features_num_ / 4;
+        local_features_num_ = param.local_features_num_ ;
     }
     else {
-        local_features_num_ = param.local_features_num_ / 8;
+        local_features_num_ = param.local_features_num_ / 4;
     }
 
 	landmark_index_ = landmark_index;
